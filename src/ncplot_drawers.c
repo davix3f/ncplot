505661748fb22ef28fbeb268367860eb52d789db
Davide <davide_fiorito@libero.it>
2018-11-13 23:06:41 +0100

    draw_* functions implementation Here every drawing function is implemented. Guess what draw_point does= :O
    Probably, draw_axes should be part of environment
    setters.

---
+++ src/ncplot_drawers.c
@@ -0,0 +1,37 @@
+#include "ncplot.h"
+
+void draw_axes()
+{
+	int max_x, max_y;
+	getmaxyx(main_win, max_y, max_x);
+	
+	for(int y_indx=1; y_indx<max_y-1; y_indx++)
+	{
+		mvwprintw(main_win, y_indx, max_x/2, "+"); 
+	}
+
+	for(int x_indx=1; x_indx<max_x-1; x_indx++)
+	{
+		if(x_indx==max_x/2)
+		{ mvwprintw(main_win, max_y/2, x_indx, "0"); }
+		else
+		{ mvwprintw(main_win, max_y/2, x_indx, "+"); }
+	}
+
+	wrefresh(main_win);
+}
+
+void draw_point(int point_x, int point_y)
+{
+	int max_x, max_y;
+	getmaxyx(main_win, max_y, max_x);
+
+	if(point_x<max_x && point_y<max_y)
+	{
+		mvwprintw(main_win, (max_y/2)-point_y, (max_x/2)+point_x, "o");
+		mvwprintw(main_win, (max_y/2)-point_y, max_x/2, "%i", point_y);
+		mvwprintw(main_win, max_y/2, (max_x/2)+point_x, "%i", point_x);
+		wrefresh(main_win);
+	}
+}
+
