fea44902e23bdfed889b728293c442dd342101f0
Davide <davide_fiorito@libero.it>
2018-11-14 23:01:58 +0100

    Added sections

--- src/ncplot.h
+++ src/ncplot.h
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <curses.h>
 
+// Useful variables
 static WINDOW* main_win;
 
+// Environment setters
 void init_plotenv();
 void end_plotenv();
 
+// Drawing functions
 void draw_axes();
 void draw_point(int point_x, int point_y);
