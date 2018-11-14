e2c4f44061b26186db9c989400791417cfd02fe5
Davide <davide_fiorito@libero.it>
2018-11-15 00:46:09 +0100

    Added point tracers
    Print - for on x axis and | for y axis 
    to 'trace' the point, like that
    		
          y|
            |--o
            |__|___x   *

--- src/ncplot_drawers.c
+++ src/ncplot_drawers.c
@@ -24,8 +24,35 @@ void draw_point(int point_x, int point_y)
 	if(point_x<max_x && point_y<max_y)
 	{
 		mvwprintw(main_win, (max_y/2)-point_y, (max_x/2)+point_x, "o");
-		mvwprintw(main_win, (max_y/2)-point_y, max_x/2, "%i", point_y);
-		mvwprintw(main_win, max_y/2, (max_x/2)+point_x, "%i", point_x);
+
+		/*
+		Print - for on x axis and | for y axis 
+		to 'trace' the point, like that
+		
+	   y|
+		|--o
+		|__|___x   */
+		
+		for(int x=(max_x/2)+1;
+			point_x > 0 ? x<(max_x/2)+point_x : x>(max_x/2)+point_x;
+			// if x>0 while(x<point_x){x++} else while(x>point_x){x--}
+			point_x > 0 ? x++ : x--)
+		{
+			mvwprintw(main_win, (max_y/2)-point_y, x, "-");
+		}
+
+		for(int y= point_y > 0 ? (max_y/2)-1 : (max_y/2) ;
+			point_y > 0 ?  y>(max_y/2)-point_y : y<(max_y/2)-point_y;
+			// if y>0 while(y>point_y){y--} else while(y<point_y){y++}
+			point_y > 0 ? y-- : y++)
+		{
+			mvwprintw(main_win, y, (max_x/2)+point_x, "|");
+		}
+
+		// Print point coordinates in absolute form on the relative axis
+		mvwprintw(main_win, (max_y/2)-point_y, max_x/2, "%i", abs(point_y)); // y
+		mvwprintw(main_win, max_y/2, (max_x/2)+point_x, "%i", abs(point_x)); // x
+
 		wrefresh(main_win);
 	}
 }
