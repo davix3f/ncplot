86a69ce6c9309cc443d06fecf09cae1f7b2e0c34
Davide <davide_fiorito@libero.it>
2018-11-19 00:46:24 +0100

    draw_point_set example added

--- src/example.c
+++ src/example.c
@@ -3,12 +3,18 @@
 
 int main()
 {
+
 	init_plotenv();
 	draw_axes();
-	draw_point(10, 10);
-	draw_point(-10, 10);
+
 	draw_point(-10, -10);
 	draw_point(10, -10);
+
+	coord_pair pairs[] = { {0, 9},
+						   {8, 6},
+						   {5, 14} };
+	draw_point_set(pairs, 3);
+
 	getch();
 	end_plotenv();
 }
